c0398f6350429714830419e8909e165e04dbc784
perellorodrigo <35689617+perellorodrigo@users.noreply.github.com>
2019-04-25 13:00:43 +1000

    Android added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+
+  constructor(private datePicker: DatePicker) {
+
+  }
+  
+  displayDatePicker(){
+    this.datePicker.show({
+      date: new Date(),
+      mode: 'date',
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+    }).then(
+      date => console.log('Got date: ', date),
+      err => console.log('Error occurred while getting date: ', err)
+    );
+  }
+  
+
+
+
+}
